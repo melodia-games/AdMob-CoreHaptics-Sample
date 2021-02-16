1d1b0dd50c3d58f32767b45022357b59943f5783
Yuko Suzuki <jp.melodia.games@gmail.com>
2021-02-16 16:31:01 +0900

    Added SoundManager to play some sound

---
+++ Assets/Main/SoundManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] AudioSource sound;
+
+    public void PlaySomeSound()
+    {
+        sound.Play();
+    }
+}
